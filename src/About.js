b9025f7dcec6feff495dee0b1fdb0d083124cbcd
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-06-01 19:38:17 +0300

    added react-router

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function About(){
+    return(
+        <div className="About">
+            <p>
+            I am a Front-End Developer, who is open minded and passionate about this field because it drives me to because
+                to be resourceful and inquisitive.
+                I taught myself how to code.
+            </p>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+
+
 
 function App() {
   return (
-    "Hello Portifolio"
+//     <div className='App'>
+//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//   <div className="container-fluid">
+//     <a className="navbar-brand" href="/">Porti<strong>folio</strong></a>
+//     <div className="collapse navbar-collapse" id="navbarNav">
+//       <ul className="navbar-nav">
+//         <li className="nav-item">
+//           <a className="nav-link active" aria-current="page" href="/">Home</a>
+//         </li>
+//         <li className="nav-item">
+//           <a className="nav-link" href="/src/About.js">About</a>
+//         </li>
+//         <li className="nav-item">
+//           <a className="nav-link" href="/">Projects</a>
+//         </li>
+//         <li className="nav-item">
+//           <a className="nav-link" href="/">Contact</a>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
+const Home = () => {
+  return
+    <div>
+      <p>Hello About</p>
+    </div>
+  
+};
+
+      <div className='row'>
+        <div className='header col-md-6'>
+                Hello 
+                I am Eunice Kimani a Front-End Developer
+                I love to create
+                
+        </div>
+        <div className='top-header col-md-6'>
+        <img src="/public/port.jpg"alt="girl-coding" width="70px" height="70px"/>
+        <div className='button'>
+          <a href='/src/About.js'>
+          <button >
+            About Me
+          </button>
+          </a>
+        </div>
+        </div>
+      </div>
+
+    // </div>
   );
 }
 
